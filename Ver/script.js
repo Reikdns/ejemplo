f2b891943c672d5a5f710b3b3800239cedd6186b
Rei <reik.dns@gmail.com>
2024-01-28 11:14:53 -0500

    Primer commit

---
+++ Ver/script.js
@@ -0,0 +1,42 @@
+var estudiantesJSON = []
+var tabla = document.getElementById("tablaDatos");
+
+const apiBase =  "http://127.0.0.1:8000"
+
+function obtenerTodos(){
+    const url = new URL(apiBase + "/obtener_todos").toString()
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        estudiantesJSON = data
+        actualizarTabla()
+    })
+    .catch(error => console.log(error))
+}
+
+
+function actualizarTabla(){
+    estudiantesJSON.forEach(function (estudiante) {
+        var fila = tabla.insertRow();
+        var celdaNombre = fila.insertCell(0);
+        var celdaIdentificacion= fila.insertCell(1);
+        var celdaSexo = fila.insertCell(2);
+        var celdaEdad = fila.insertCell(3);
+        var celdaPulsaciones = fila.insertCell(4)
+
+    
+        celdaNombre.textContent = estudiante.nombre
+        celdaIdentificacion.textContent = estudiante.identificacion
+        celdaSexo.textContent = estudiante.sexo
+        celdaEdad.textContent = estudiante.edad
+        celdaPulsaciones.textContent = "Masculino" == estudiante.sexo ? (220-estudiante.edad) : (226-estudiante.edad)
+
+      });
+}
+
+ 
+
+document.addEventListener('load', obtenerTodos())
+
+
+
